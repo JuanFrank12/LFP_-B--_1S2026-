520025e1ca4d8f86a7f02990fa02ae17e99d6fc3
JUANFRANK2022 <140723188+JuanFrank12@users.noreply.github.com>
2026-05-01 19:10:22 -0600

    correccion
    a

--- PRACTICA1/src/services/FileReader.cpp
+++ PRACTICA1/src/services/FileReader.cpp
@@ -6,6 +6,7 @@
 
 std::vector<Estudiante> FileReader::leerEstudiante(const std::string& ruta){
     std::vector<Estudiante> lista;
+    std::vector<Estudiante2> lista;
     std::ifstream archivo(ruta);
 
     if()
